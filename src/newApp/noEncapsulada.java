f01604274c11dac91812b58fd8f87a05ee882c37
Tarde.A3 <Tarde.A3@LAPTOP-KIU3UVD5>
2019-07-26 20:25:22 +0200

    hoy estamos con los metodos de encapsulacion

---
+++ src/newApp/noEncapsulada.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newApp;
+
+/**
+ *
+ * @author Tarde.A3
+ */
+public class noEncapsulada {
+
+    /**
+     * @param args the command line arguments
+     */
+        // ATRIBUTOS
+        
+        public int hora;
+        public int  minutos;
+    
+    
+}
